e75c0f74c137c21f6304df0321f3f13a2d64ed94
Jeremy Tarling <jeremy.tarling@gmail.com>
2012-02-19 15:10:04 +0000

    added keywords filter

--- text-processor.rb
+++ text-processor.rb
@@ -5,17 +5,21 @@ line_count = lines.size
 text = lines.join
 total_characters = text.length
 total_characters_nospace = text.gsub(/\s+/, '').length
-word_count_with_split = text.split.length
+words = text.split
+word_count = text.split.length
 sentence_count = text.split(/\.|\?|!/).length
 para_count = text.split(/\n\n/).length
+stopwords = %w{the a by on for of are with just but add to my I has some in}
+keywords = words.select { |word| !stopwords.include?(word) }
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
 puts "#{total_characters_nospace} characters excluding spaces"
-puts "#{word_count_with_split} words"
+puts "#{word_count} words"
+puts "#{keywords.length} keywords (excluding: the a by on for of are with just but add to my I has some in)"
 puts "#{sentence_count} sentences"
 puts "#{para_count} paragraphs"
 puts "#{sentence_count / para_count} sentecnes per paragraph (average)"
-puts "#{word_count_with_split / sentence_count} words per sentence (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
 
 
