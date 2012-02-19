9a90c096fdc7c4cbd58093f38e9b1da794c673a4
Jeremy Tarling <jeremy.tarling@gmail.com>
2012-02-19 19:32:59 +0000

    added % calculation for keywords

--- text-processor.rb
+++ text-processor.rb
@@ -12,14 +12,17 @@ para_count = text.split(/\n\n/).length
 stopwords = %w{the a by on for of are with just but add to my I has some in}
 keywords = words.select { |word| !stopwords.include?(word) }
 
+
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
 puts "#{total_characters_nospace} characters excluding spaces"
 puts "#{word_count} words"
-puts "#{keywords.length} keywords (excluding: the a by on for of are with just but add to my I has some in)"
 puts "#{sentence_count} sentences"
 puts "#{para_count} paragraphs"
 puts "#{sentence_count / para_count} sentecnes per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{keywords.length} keywords (words excluding: the, a, to, etc.)"
+puts "#{ ((keywords.length.to_f / words.length.to_f) * 100).to_i} % of words are keywords"
+
 
 
